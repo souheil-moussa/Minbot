aff6c898d987e34c52e72c6e693a0689b96d6a0f
souheil-moussa <souheil.imoussa@gmail.com>
2025-03-27 13:38:47 +0200

    improved website

--- controller/wss/index.js
+++ controller/wss/index.js
@@ -2,16 +2,22 @@ const Websocket = require("ws");
 const { exec } = require('child_process');
 const wss = new Websocket.Server({ port: 8081 });
 console.log("Websocket server started");
+var ip = null
 wss.on("connection", ws => {
 	console.log("connected");
 
 	ws.on("message", data => {
 		if (`${data}` == 'start') {
-			exec('./stream_start.sh', error => {
-				if (error) {
-					console.log(`${error}`);
-				}
-			});
+			if (ip == null) {
+				console.error("server IP was not recived");
+			}
+			else {
+				exec('./stream_start.sh', [ip], error => {
+					if (error) {
+						console.log(`${error}`);
+					}
+				});
+			}
 		}
 		else if (`${data}` == 'stop') {
 			exec('./stream_stop.sh', error => {
@@ -19,6 +25,13 @@ wss.on("connection", ws => {
 					console.log(`${error}`);
 				}
 			});
+		}
+		else if (isJsonString(`${data}`)) {
+			const mdata = JSON.parse(`${data}`);
+
+			if (mdata.serverIP) {
+				ip = mdata.serverIP;	//edit stream ip
+			}
 		}
 	});
 
@@ -28,4 +41,11 @@ wss.on("connection", ws => {
 	});
 });
 
-
+function isJsonString(str) {
+	try {
+		JSON.parse(str);
+	} catch (e) {
+		return false;
+	}
+	return true;
+}
